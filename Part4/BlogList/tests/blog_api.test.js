8734b0bee02f5827b5e764d7a525662aac0b919a
Juuso <juusosavolainen1@kamk.fi>
2024-12-07 23:37:54 +0200

    starting 4.8

---
+++ Part4/BlogList/tests/blog_api.test.js
@@ -0,0 +1,13 @@
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const assert = require('node:assert')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    
+})
